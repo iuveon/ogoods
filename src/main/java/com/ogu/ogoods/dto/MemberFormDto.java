42e537b08685ec0fad2263af38aa1cfc23f09ef1
JOJUYEON <ioiuveon@gmail.com>
2023-10-31 16:12:47 +0900

    231031 * 회원가입 유효성 검사 추가  - 비밀번호, 이메일, 연락처 유효성 추가 * 상품 상세 페이지 수정  - 하단 상품 상세 설명 부분 수정  - 하단 배송 및 교환 반품 정보 추가 * 상품 목록 페이지 수정  - 목록에서는 상품 상세설명이 노출되지 않음  - 상품명의 길이가 길 경우 말 줄임표와 함께 한 줄만 출력되도록 수정

--- src/main/java/com/ogu/ogoods/dto/MemberFormDto.java
+++ src/main/java/com/ogu/ogoods/dto/MemberFormDto.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
@@ -12,21 +13,25 @@ public class MemberFormDto {
 
     @NotBlank(message = "아이디는 필수 입력 값입니다.")
     // NotBlank : Null값 비허용 ("", " " 모두 비허용)
-    @Length(min=4, max=12, message = "아이디는 4자 이상, 12자 이하로 입력해주세요")
+    @Length(min=4, max=12, message = "아이디는 4자 이상, 12자 이하로 입력해주세요.")
     // @Length : 필드 크기 검증
     private String mid;
 
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
-    @Length(min=8, max=20, message = "비밀번호는 8자 이상, 20자 이하로 입력해주세요")
+    @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
+            message = "비밀번호는 영어와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다.")
+    // @Length(min=8, max=20, message = "비밀번호는 8자 이상, 20자 이하로 입력해주세요")
     private String password;
 
     @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String name;
 
     @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식에 맞지 않습니다.")
     private String email;
 
     @NotBlank(message = "연락처는 필수 입력 값입니다.")
+    @Pattern(regexp = "(01[016789])(\\d{3,4})(\\d{4})", message = "올바른 휴대폰 번호를 입력해주세요.")
     private String phone;
 
     @NotBlank(message = "주소는 필수 입력 값입니다.")
